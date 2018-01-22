7daaf22d4daea3b22a98ed1495fd8b68ee1300dd
Jan Michael Gardose <jan.michael.gardose@gmail.com>
2018-01-22 11:36:00 +0900

    change map location
    change map location

--- assets/js/gmap.js
+++ assets/js/gmap.js
@@ -2,12 +2,12 @@ function initMap() {
     var locations = [
 		['2-24-13, Jinden-cho Kandamachi, Miyako-gun, Fukouka-ken Japan', 33.792722, 130.980963, 3],
 		['7F Fukuhisa Building 4-49 MinamiNakadori, Nakaku,Yokohama-shi Japan', 35.448711, 139.637684, 2],
-		['5F King\'s Court Building 1, Makati, Chino Roces Avenue, Makati, NCR, Philippines', 14.5576173, 121.0112967, 1]
+		['5F King\'s Court Building 1, Makati, Chino Roces Ave Legazpi Village, Makati, NCR, Philippines', 14.557922, 121.013412, 1]
 	];
   
 	var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 4,
-		center: new google.maps.LatLng(27.617705, 127.743045),
+		center: new google.maps.LatLng(27.617705, 133.743045),
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	});
   
