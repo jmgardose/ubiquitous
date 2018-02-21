f4aea3077b0aeb92520d6903d48b4a1ae8047cc2
Gian Mina <gsmina@ubiquitous-tech.com>
2018-02-21 16:56:08 +0800

    Implemented an on-scroll effect for the navbar to enhance UX.

---
+++ assets/js/header-scroll.js
@@ -0,0 +1,21 @@
+
+window.onload = function () {
+
+    var navbar = document.getElementById('header');
+    var sticky = navbar.offsetTop;
+
+    window.onscroll = function() {
+        // console.log('Just keep scrolling');
+        var scrollTop = document.documentElement.scrollTop 
+                     || document.body.scrollTop;
+
+        if (window.pageYOffset >= sticky) {
+            document.getElementById('header').classList.add("scroll-header");
+            document.body.classList.add("subpage");
+        }
+        else {
+            document.getElementById('header').classList.remove("scroll-header");
+            document.body.classList.remove("subpage");
+        }
+    };
+}
