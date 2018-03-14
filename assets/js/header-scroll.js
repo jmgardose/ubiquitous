bf071b4533f18abe089fd402148b1bddff365f86
Gian Mina <gsmina@ubiquitous-tech.com>
2018-03-14 15:07:38 +0800

    [Header Bar] Change header bar scroll effect
    Change header bar scroll effect since the other one had a weird transition effect.

--- assets/js/header-scroll.js
+++ assets/js/header-scroll.js
@@ -1,19 +1,24 @@
 window.onload = function () {
 
     var navbar = document.getElementById('header');
+    var banner = document.getElementById('banner');
     var sticky = navbar.offsetTop;
 
     window.onscroll = function () {
-        // console.log('Just keep scrolling');
         var scrollTop = document.documentElement.scrollTop
             || document.body.scrollTop;
 
-        if (window.pageYOffset >= sticky) {
+        if (window.pageYOffset >= sticky * 4) {
             document.getElementById('header').classList.add("scroll-header");
-            document.body.classList.add("subpage");
         }
         else {
             document.getElementById('header').classList.remove("scroll-header");
+        }
+
+        if (window.pageYOffset >= banner.offsetTop + banner.offsetHeight) {
+            document.body.classList.add("subpage");
+        }
+        else {
             document.body.classList.remove("subpage");
         }
     };
