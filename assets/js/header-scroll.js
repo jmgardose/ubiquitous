9946a3beeef5a2184044fa34717d7fe85c06175e
Gian Mina <gsmina@ubiquitous-tech.com>
2018-03-14 17:32:40 +0800

    [Header Bar] Implement a new header bar scroll mechanic
    Implement a new header bar scroll mechanic where the header bar will only appear when scrolling up.
    
    Adjust the navbar in order to give equal width to all links.

--- assets/js/header-scroll.js
+++ assets/js/header-scroll.js
@@ -4,22 +4,32 @@ window.onload = function () {
     var banner = document.getElementById('banner');
     var sticky = navbar.offsetTop;
 
+    var prevScrollTop = -1;
+
     window.onscroll = function () {
         var scrollTop = document.documentElement.scrollTop
             || document.body.scrollTop;
 
         if (window.pageYOffset >= sticky * 4) {
             document.getElementById('header').classList.add("scroll-header");
-        }
-        else {
+            document.getElementById('header').classList.add("hide-header");
+        } else {
             document.getElementById('header').classList.remove("scroll-header");
+            document.getElementById('header').classList.remove("hide-header");
+            document.body.classList.remove("subpage");
         }
 
         if (window.pageYOffset >= banner.offsetTop + banner.offsetHeight) {
             document.body.classList.add("subpage");
+
+            // If scrolling down
+            if (scrollTop > prevScrollTop) {
+                document.getElementById('header').classList.add("hide-header");
+            } else {
+                document.getElementById('header').classList.remove("hide-header");
+            }
         }
-        else {
-            document.body.classList.remove("subpage");
-        }
+
+        prevScrollTop = scrollTop;
     };
 }
